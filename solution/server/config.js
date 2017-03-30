4c86422e6ab1371528fac5e961a3a76ce7c361c5
barzev <petar.barzev@gmail.com>
2017-03-30 18:25:04 +0100

    jjjjjjjtlat

---
+++ solution/server/config.js
@@ -0,0 +1,8 @@
+const configData = {
+  consumer_key: 'wOfPNRD7TlxVezOj9njxt8dxm',
+  consumer_secret: 'KkVMEr0MufvDqjliMjb05pjGPDSb6lDYQs5KryNZPh6wLTno7L',
+  access_token_key: '310511015-UnQmhSdUbPJJ8tegTcMjm2BbI5tuyERWx9Aibn6D',
+  access_token_secret: 'BhgH5FtTX5ZpK1K3yPPWVKohCfWjRMnKodgnRl9rIshUA'
+}
+
+module.exports = configData;
