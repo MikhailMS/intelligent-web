4bd0fd8c8ab1f3aa5912a305d5b985f79af97dc3
barzev <petar.barzev@gmail.com>
2017-03-30 20:11:19 +0100

    config file added, dependencies fixed

---
+++ solution/server/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pbarzev on 30/03/2017.
+ */
+var config = {
+  consumer_key: 'wOfPNRD7TlxVezOj9njxt8dxm',
+  consumer_secret: 'KkVMEr0MufvDqjliMjb05pjGPDSb6lDYQs5KryNZPh6wLTno7L',
+  access_token_key: '310511015-UnQmhSdUbPJJ8tegTcMjm2BbI5tuyERWx9Aibn6D',
+  access_token_secret: 'BhgH5FtTX5ZpK1K3yPPWVKohCfWjRMnKodgnRl9rIshUA'
+};
+
+module.exports = config;
