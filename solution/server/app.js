589f34e584bd5eab8d888d72ff7083a9723ffbf6
Blagoslav Mihaylov <blago.mihailov@gmail.com>
2017-03-31 05:27:39 +0100

    fixed eventemitter overflow

--- solution/server/app.js
+++ solution/server/app.js
@@ -26,12 +26,12 @@ var routes = require('./routes/index');
 // Set up Twitter API ==================
 // =======================
 var twit = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
-}),
-stream = null;
+        consumer_key: config.consumer_key,
+        consumer_secret: config.consumer_secret,
+        access_token_key: config.access_token_key,
+        access_token_secret: config.access_token_secret
+    }),
+    stream = null;
 
 // =======================
 // Application configuration ===========
@@ -59,27 +59,27 @@ app.use('/', routes);
 // Error handlers ======================
 // =======================
 app.use(function(req, res, next) { // Catch 404 and forward to error handler
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 
 if (app.get('env') === 'development') { // Development error handler will print stacktrace
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 app.use(function(err, req, res, next) { // Production error handler no stacktraces leaked to user
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 // =======================
@@ -94,132 +94,74 @@ io.sockets.on('connection', function (socket) {
 
         var track = "";
 
-        for(var key in users)
-            for(var s in users[key])
-                track += users[key][s] + ",";
-        track = track.substring(0, track.length - 1);
-        console.log(track);
+        twit.stream('statuses/filter', { track: track }, function(stream) {
+            /*stream.on('data', function(streamData) {
+                var text = null;
+                if (streamData.text) {
+                    text = streamData.text;
+                }
+                console.log(text);
+                //var tweetData = { "msg": text };
+                //socket.broadcast.emit("twitter-stream", tweetData);
+            });*/
+        });
 
-        for(var sock in users) {
-            for(var sess in users[sock]) {
-                var tweetData = {"msg": users[sock][sess], "session": sess};
-                socket.emit('twitter-stream', JSON.stringify(tweetData));
+            for(var key in users)
+                for(var s in users[key])
+                    track += users[key][s] + ",";
+            track = track.substring(0, track.length - 1);
+            console.log(track);
+
+            for(var sock in users) {
+                for(var sess in users[sock]) {
+                    var tweetData = {"msg": users[sock][sess], "session": sess};
+                    socket.emit('twitter-stream', JSON.stringify(tweetData));
+                }
             }
         }
-        /*twit.stream('statuses/filter', {'locations':'-180,-90,180,90', 'track': track}, function(stream) {
-            stream.on('data', function(data) {
-                var text = null;
 
-                if (data.text) {
-                    text = data.text;
+            var session;
+            socket.on('start-streaming', function(data) {
+                if(users[socket] == null)
+                    users[socket] = {};
+
+                data = JSON.parse(data);
+
+                if(data['session'] in users[socket]) {
+                    session = data['session'];
+                } else {
+                    session = Math.random().toString(36).substr(2,16);
                 }
-                // Create JSON object that would be send to client
 
-                //console.log(text);
+                users[socket][session] = data['keyword'];
 
-                for(var sock in users) {
-                    for(var sess in users[sock]) {
-                        if(text !== null)
-                            if(text.includes(users[sock][sess])) {
-                                var tweetData = {"msg": text, "session": session};
-                                socket.emit('twitter-stream', tweetData);
-                            }
-                    }
+                socket.emit('receive-session', session);
+                resetStream();
+
+                //for(var key in users)
+                // console.log(users[key]);
+            });
+
+            socket.on('disconnect', function() {
+                if(session != null) {
+                    delete users[socket][session];
+                    //users[socket] = users[socket].filter(function(i) {return i != session});
+                    if (users[socket] == null)
+                        delete users[socket];
+                    resetStream();
                 }
             });
 
-            stream.on('limit', function(limitMessage) {
-                return console.log(limitMessage);
+            // Emits signal to the client telling them that the
+            // they are connected and can start receiving Tweets
+            socket.emit("connected");
+        });
+
+            // =======================
+            // Start an application ================
+            // =======================
+            http.listen(port, function(){
+                console.log('App listening at http://localhost:'+port);
             });
-
-            stream.on('warning', function(warning) {
-                return console.log(warning);
-            });
-
-            stream.on('disconnect', function(disconnectMessage) {
-                return console.log(disconnectMessage);
-            });
-        });*/
-    }
-
-    var session;
-    socket.on('start-streaming', function(data) {
-        if(users[socket] == null)
-            users[socket] = {};
-
-        data = JSON.parse(data);
-
-        if(data['session'] in users[socket]) {
-            session = data['session'];
-        } else {
-            session = Math.random().toString(36).substr(2,16);
-        }
-
-        users[socket][session] = data['keyword'];
-
-        socket.emit('receive-session', session);
-        resetStream();
-
-        //for(var key in users)
-           // console.log(users[key]);
-    });
-
-    /*socket.on("start tweets", function() {
-
-    if(stream === null) {
-      // Connect to twitter stream passing in filter for entire world.
-      twit.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
-
-          stream.on('data', function(data) {
-
-              var text = null;
-
-              if (data.text) {
-                text = data.text;
-              }
-              // Create JSON object that would be send to client
-              var tweetData = {"msg": text};
-
-              // Send data to client
-              socket.broadcast.emit("twitter-stream", tweetData);
-              socket.emit('twitter-stream', tweetData);
-          });
-
-          stream.on('limit', function(limitMessage) {
-              return console.log(limitMessage);
-          });
-
-          stream.on('warning', function(warning) {
-              return console.log(warning);
-          });
-
-          stream.on('disconnect', function(disconnectMessage) {
-              return console.log(disconnectMessage);
-          });
-      });
-    }
-  });*/
-
-  socket.on('disconnect', function() {
-      if(session != null) {
-          delete users[socket][session];
-          //users[socket] = users[socket].filter(function(i) {return i != session});
-          if (users[socket] == null)
-              delete users[socket];
-          resetStream();
-      }
-  });
-
-  // Emits signal to the client telling them that the
-  // they are connected and can start receiving Tweets
-  socket.emit("connected");
-});
-
-// =======================
-// Start an application ================
-// =======================
-http.listen(port, function(){
- console.log('App listening at http://localhost:'+port);
-});
 
 //module.exports = app;
